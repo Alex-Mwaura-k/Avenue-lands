f23a479317b03225af94bfd6ce27ea0d015e02bd
robert <kariukir057@gmail.com>
2026-02-28 19:23:55 +0300

    update

--- src/data/propertiesData.js
+++ src/data/propertiesData.js
@@ -94,7 +94,7 @@ export const properties = [
     status: "Available",
 
     img: "/img/properties/Zawadi-garden-Matuu/main.png",
-    images: ["/img/properties/Zawadi-garden-Matuu/main.png"],
+    images: ["/img/properties/Zawadi-garden-Matuu/1.png"],
 
     description:
       "Our most affordable project yet. Located 10 minutes from Matuu CBD. Perfect for investors looking to secure prime land cheaply. The area is developing rapidly with new infrastructure projects underway. With ready title deeds, electricity, and community water, it's an excellent opportunity to own land in rapid growing Matuu town.",
