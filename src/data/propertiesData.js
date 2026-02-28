18d42d64ad7d82d88c6ab6ee94d8b7edc9527fab
robert <kariukir057@gmail.com>
2026-02-28 18:59:55 +0300

    update

--- src/data/propertiesData.js
+++ src/data/propertiesData.js
@@ -68,9 +68,9 @@ export const properties = [
     price: "500,000",
     status: "Available",
 
-    img: "/img/properties/unity-gardens/main.png",
+    img: "/img/properties/breeze-gardens/main.png",
 
-    images: ["/img/properties/unity-gardens/main.png"],
+    images: ["/img/properties/breeze-gardens/main.png"],
 
     description:
       "Prime commercial and residential plots in the heart of Makutano(ya Mwea). Extremely high value appreciation rate. Ideal for building rental flats or commercial centers. The area is a bustling hub with a vibrant New Makutano market, schools, and easy access to the Kenol-Marua dual carriage highway. With ready title deeds, water, electricity, and a secure perimeter fence, Unity Garden offers everything you need for a successful investment.",
